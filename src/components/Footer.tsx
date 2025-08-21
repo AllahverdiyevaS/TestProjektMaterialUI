1d0179499348a844c336de286f64b37826b3ef16
Allahverdiyeva <Allahverdiyeva@dataflor.de>
2025-08-21 14:56:12 +0200

    'init3'

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,16 +7,23 @@ const Footer = () => {
       component="footer"
       sx={{
         position: "fixed",
-        bottom: 0,
+        bottom: 4,
+        marginBlockEnd: 1,
+
         display: "flex",
         width: "100%",
         justifyContent: "space-between",
       }}
     >
-      <Typography>GreenGarden</Typography>
-      <Box>
-        <Link>Privacy Policy</Link>
-        <Link>Terms and Conditions</Link>
+      <Typography sx={{ color: "#494444ff" }}>GreenGarden</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          gap: 2,
+        }}
+      >
+        <Link sx={{ color: "#494444ff" }}>Privacy Policy</Link>
+        <Link sx={{ color: "#494444ff" }}>Terms and Conditions</Link>
       </Box>
     </Box>
   );
